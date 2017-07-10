1743d476135a1ed2f4ef268a4c4dd7fbbd97d510
EC2 Default User <ec2-user@ip-172-31-23-134.us-east-2.compute.internal>
2017-07-10 16:40:38 +0000

    added fields.config file and config parsing functionality

--- mesqticker.py
+++ mesqticker.py
@@ -5,18 +5,65 @@ import os
 import sys
 import argparse
 from elasticsearch import Elasticsearch
+import configparser
+import decimal
+import json
+import logging
+import pprint
+import time
+import urllib
 
 parser = argparse.ArgumentParser(description="Returns Elasticsearch documents matching ticker query")
 parser.add_argument("-i", "--index", required=True, help="name of ES index to query")
 parser.add_argument("-t", "--ticker", required=True, help="Ticker to query")
 
-args = parser.parse_args(sys.argv[1:])
+largs = parser.parse_args(sys.argv[1:])
 
-dex = args.index
-tick = args.ticker
+dex = largs.index
+tick = largs.ticker
+
 
 client = Elasticsearch()
 
+def getConfig():
+	parser = argparse.ArgumentParser(description='Poll Quandl for dataset export status')
+	parser.add_argument('--config-file', '-cf', dest='configFile', required=False, default='fields.config')
+	parser.add_argument('--config-environment', '-env', dest='configEnv', required=False, default='dev')
+        parser.add_argument("-i", "--index", required=True, dest='dex', help="name of ES index to query")
+        parser.add_argument("-t", "--ticker", required=True, dest='tick', help="Ticker to query")
+	args = parser.parse_args()
+	cfg = configparser.ConfigParser()
+	cfg.read(args.configFile)
+	config = cfg[args.configEnv]
+	fields = {x.strip() for x in config['rfields'].split(',')}
+	config.fields = fields
+	return config
+
+def getTick(dex, tick):
+    response = client.search(
+        index = dex,
+        body = {"query":{"multi_match":{"query":tick,"fields":"_all"}}}
+    )
+    config = getConfig()
+    for hit in response['hits']['hits']:
+        for field in config.fields:
+            print(hit['_score'], hit['_source'][field])
+
+
+#        return;
+#    return;
+
+def main():
+    getTick(dex, tick)
+
+main()
+
+
+    
+
+
+
+'''
 def gettick(dex, tick, *fields):
 	for field in fields:
 		response = client.search(
@@ -28,3 +75,4 @@ def gettick(dex, tick, *fields):
 		return;
 	return;
 gettick(dex,tick,'contentText','tickers')
+'''
